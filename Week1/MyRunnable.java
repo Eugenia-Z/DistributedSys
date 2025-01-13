6149b3a3a0ff52ce2902c285edb2022011e04a40
eugenia <e.eugene1201@gmail.com>
2025-01-12 22:41:06 -0800

    Added java threads

---
+++ Week1/MyRunnable.java
@@ -0,0 +1,19 @@
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyRunnable());
+        Thread t2 = new Thread(new MyRunnable());
+        t1.start(); // when t1 is started, it should execute the run() method defined in the MyRunnable (in a separate thread.)
+        t2.start();
+    }
+}
