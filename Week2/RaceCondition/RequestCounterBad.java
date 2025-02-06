ac7cb7fe7a8120f46e695c4f0611946f768d53d7
eugenia <e.eugene1201@gmail.com>
2025-02-06 13:56:36 -0800

    Added race condition examples

---
+++ Week2/RaceCondition/RequestCounterBad.java
@@ -0,0 +1,24 @@
+public class RequestCounterBad {
+    private static final int NUM_THREADS = 100000;
+    private int count = 0; // shared vairable for all threads
+
+    public void inc() {
+        count++;
+    }
+
+    public int getVal() {
+        return count;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final RequestCounterBad counter = new RequestCounterBad();
+        for (int i = 0; i < NUM_THREADS; i++) {
+            Runnable thread = () -> {
+                counter.inc();
+            };
+            new Thread(thread).start();
+        }
+        Thread.sleep(5000); // main thread sleeps
+        System.out.println("Value should be " + NUM_THREADS + " - it is: " + counter.getVal());
+    }
+}
