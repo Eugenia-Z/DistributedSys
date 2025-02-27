3192c92f3fa7fe1c37a246e0aac6f5d1aa2a1532
eugenia <e.eugene1201@gmail.com>
2025-02-27 12:25:24 -0800

    updated double precision

--- Notes/DesignPattern/DecoratorExample.java
+++ Notes/DesignPattern/DecoratorExample.java
@@ -85,12 +85,12 @@ public class DecoratorExample {
 
         Beverage latte = new Latte();
         latte = new Sugar(new Sugar(new Milk(latte))); // add two sugar and oen milk
-        System.out.println(latte.getDescription() + " $" + latte.cost());
+        System.out.println(latte.getDescription() + " $" + String.format("%.2f", latte.cost()));
     }
 }
 
 /*
- * 为什么 CondimentDecorator 需要 abstract String getDescription();
+ * 1. 为什么 CondimentDecorator 需要 abstract String getDescription();
  * 
  * CondimentDecorator 作为装饰器，需要修改 getDescription() 的行为，即它的子类（如 Milk、Sugar）需要
  * 拼接额外的描述。既然 CondimentDecorator 不能提供通用的 getDescription() 实现，就强制子类实现，所以把它声明为
@@ -103,4 +103,8 @@ public class DecoratorExample {
  * CondimentDecorator 不是必须要定义 abstract getDescription();，但这么做有助于代码清晰性：
  * ✅ 定义 abstract getDescription();，强制子类实现
  * ✅ 不定义 abstract cost();，因为 Beverage已经定义了，子类必须实现
+ * 
+ * 
+ * 2. 为什么latte 打印价格为3.4000000000000004
+ * Java 采用 IEEE 754 浮点数标准来存储 double，由于浮点数不能精确存储某些小数，导致计算时出现 微小误差。
  */
