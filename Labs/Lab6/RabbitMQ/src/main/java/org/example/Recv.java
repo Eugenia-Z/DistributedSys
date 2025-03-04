ca4f86c6164c5ea0d804e6bcc5576e4634da745d
eugenia <e.eugene1201@gmail.com>
2025-03-04 12:46:58 -0800

    added comments for RMQ lab6

--- Labs/Lab6/RabbitMQ/src/main/java/org/example/Recv.java
+++ Labs/Lab6/RabbitMQ/src/main/java/org/example/Recv.java
@@ -25,3 +25,20 @@ public class Recv {
 
     }
 }
+
+/*
+1. DeliverCallback is used to buffer the messages pushed to us by the server
+2. Why not use a try-with-resource statement to automatically close the channel & connection? -> because we want the process to stay alive while the consumer is listening asynchronously for messages to arrive.
+3. Since the server pushes messages asynchronously, we use a callback in the form of na object that will buffer the message until we're ready to use them. (done in deliverCallback.)
+
+
+- DeliverCallback is a Lambda expression, 相当于实现了DeliverCallback interface. 当队列有消息时，RabbitMQ会自动调用deliverCallback处理消息
+- 参数consumerTag是消费者的唯一标识符，RMQ自动生成；delivery包含消息的元数据和消息体。
+- delivery.getBody() captures the content of the message (byte[])
+- new String(delivery.getBody(), "UTF-8") converts byte array to UTF-8 encoded String
+- channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+- 当前通道channel监听RMQ queue_name队列，并使用 deliverCallback 处理收到的消息。
+- true -> 自动应答，无需手动ack （if false，则需要 channel.basicAck() 确认消息-> channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);）
+- deliverCallback: 当有消息时的回调函数，负责处理消息内容
+- consumerTag -> {} : 取消监听的回调；此处空实现，表示不处理取消事件
+ */
--- Labs/Lab6/RabbitMQ/src/main/java/org/example/Send.java
+++ Labs/Lab6/RabbitMQ/src/main/java/org/example/Send.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
@@ -8,21 +9,32 @@ import java.util.concurrent.TimeoutException;
 
 public class Send {
     private final static String QUEUE_NAME = "hello";
+
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
-            // Declaring a queue is idempotent - it will only be created if it doesn't exist already.
+            // Declaring a queue is idempotent - it will only be created if it doesn't exist
+            // already.
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World";
+            String message = "Hello Kitty";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes()); // Message content is a byte array.
             System.out.println(" [x] Sent '" + message + "'");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (TimeoutException e) {
+        } catch (IOException | TimeoutException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
+/*
+ * 1. Connection: abstracts the socket connection & takes care of protocol
+ * version negotiation and authentication and so on for us.
+ * 2. Both Connection and Channel implement Java.lang.AutoCloseable. this way we
+ * don't need to close them explicitly in our code.
+ * 3. To send, we then declare a queue for us to send to; then publish a message
+ * to the queue. All done in try with resources statement.
+ * 4. Declaring a queue is idempotent - it will only be created if it doesn't
+ * exist already.
+ */
