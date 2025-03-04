c301bec4df7eeac6cb5ca4e9ec808e770298a0ea
eugenia <e.eugene1201@gmail.com>
2025-03-04 13:36:44 -0800

    added comments to task_queues

--- Labs/Lab6/RabbitMQ/src/main/java/org/example/NewTask.java
+++ Labs/Lab6/RabbitMQ/src/main/java/org/example/NewTask.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
+import java.nio.charset.StandardCharsets;
+
 public class NewTask {
 
     private static final String TASK_QUEUE_NAME = "task_queue";
@@ -20,8 +22,17 @@ public class NewTask {
 
             channel.basicPublish("", TASK_QUEUE_NAME,
                     MessageProperties.PERSISTENT_TEXT_PLAIN,
-                    message.getBytes("UTF-8"));
+                    message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
 }
+
+/*
+Message Durability
+1. Queue persistence: in channel.queueDeclare(), set durable to true -> so that the queue will survive a RabbitMQ node restart.
+ --> note that: RabbitMQ doesn't allow you to redefine an existing queue with different parameters and will return an error to any program that tries to do that.
+2. Message Persistence: set MessageProperties to PERSISTENT_TEXT_PLAIN in channel.basicPublish()
+ --> note: although it tells RabbitMQ to save the message to disk, there is still a short time window when RabbitMQ has accepted a message and hasn't saved it yet.
+ --> Stronger Guarantee: publisher confirms.
+ */
--- Labs/Lab6/RabbitMQ/src/main/java/org/example/Worker.java
+++ Labs/Lab6/RabbitMQ/src/main/java/org/example/Worker.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 public class Worker {
 
     private static final String TASK_QUEUE_NAME = "task_queue";
@@ -17,10 +19,10 @@ public class Worker {
         channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        channel.basicQos(1);
+        channel.basicQos(1); // accept only one unack-ed message at a time
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
 
             System.out.println(" [x] Received '" + message + "'");
             try {
@@ -45,3 +47,20 @@ public class Worker {
         }
     }
 }
+
+/*
+output:
+ [*] Waiting for messages. To exit press CTRL+C
+ [x] Received ''
+ [x] Done
+ [x] Received 'Hello RabbitMQ'
+ [x] Done
+RabbitMQ 的消息传递是异步的，如果消费者的 basicConsume() 方法被调用并且此时队列为空，那么有时消费者的处理逻辑可能会接收到一个空字符串。
+这通常是因为某些特定的队列配置（例如 autoAck 设置），或者消费队列中的第一个空白消息。
+
+Round-robin dispatching:
+- By default, RabbitMQ will send each message to the next consumer, in sequence.
+
+Acknowledgement:
+- Acks must be sent on the same channel that received the delivery. Attempt to acknowledge using a different channel will result in a channel-level protocol exception.
+ */
