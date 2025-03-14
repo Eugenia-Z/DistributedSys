5581288b0c89f4b97a5c1ef5466c4e5128321324
eugenia <e.eugene1201@gmail.com>
2025-03-13 17:47:03 -0700

    Added builder pattern

---
+++ Notes/DesignPattern/BuilderPattern.java
@@ -0,0 +1,75 @@
+/*
+ * The Builder Pattern is a creational design pattern that provides a way to construct complex objects step by step. It separates the construction of an object from its representatin, allowing the same construction process to create different representations. 
+ 
+ Characteristics
+
+ Encapsulation of Construction Logic 
+ – Instead of having a large constructor with multiple parameters, the builder pattern uses a step-by-step approach to configure an object.
+
+ Fluent Interface 
+ – The pattern often uses method chaining to improve readability and usability.
+
+ Immutable Object Creation
+ -– The final object is typically immutable once built.
+
+
+ Components:
+ 1. Product(Complex Object)
+ 2. Builder Interface(Abstract Builder) - defines the building steps
+ 3. Concrete Builder - implements the steps to construct the object
+ 4. Direction - Manages the construction process
+ */
+
+// product class
+class Car {
+    private String engine;
+    private int wheels;
+    private boolean sunroof;
+
+    private Car(CarBuilder builder) {
+        this.engine = builder.engine;
+        this.wheels = builder.wheels;
+        this.sunrof = builder.sunroof;
+    }
+
+    public static class CarBuilder {
+        private String engine;
+        private int wheels;
+        private boolean sunroof;
+
+        public CarBuilder setEngine(String engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        public CarBuilder setWheels(int wheels) {
+            this.wheels = wheels;
+            return this;
+        }
+
+        public CarBuilder setSunroof(boolean sunroof) {
+            this.sunroof = sunroof;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car [engine=" + engine + ", wheels=" + wheels + ", sunroof=" + sunroof + "]";
+    }
+}
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+        Car car = new Car.CarBuilder()
+                .setEngine("V8")
+                .setWheels(4)
+                .setSuroof(true)
+                .build();
+        System.out.println(car);
+    }
+}
--- Notes/Misellenuous/samples.md
+++ Notes/Misellenuous/samples.md
@@ -1,2 +1,6 @@
 https://github.com/miaoxu0414/6650/tree/main
 https://github.com/colinshenc/Deeper-Understanding-of-Vector-Clocks-and-Implementation
+https://github.com/ZinnLiu233/6650Assignment/tree/main/A2
+https://github.com/1281888304/6650Assignment2Consumer/blob/main/src/main/java/assignment2Consumer/RedisConsumerTask.java
+https://github.com/shijie09/6650Assignments/blob/main/assignment3/Consumer1/src/main/java/Consumer1.java
+https://github.com/Hannahyuting/Assignment3-SkierConsumer/blob/main/src/main/java/Consumer.java
